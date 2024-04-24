1125e766c3cf25e7d0e354639e1f7ea4bdb280a6
Robert Tea <robteaw@gmail.com>
2024-04-23 22:46:58 -0500

    assets files added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import chatgptLogo from "./assets/chatgpt.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="sidebar">
         <div className="upperSideTop">
-          <img src="" alt="" className="logo" />
+          <img src={chatgptLogo} alt="" className="logo" />
           <span className="brand">AiChat</span>
           <button className="midBtn">
             <img src="" alt="" className="addBtn" />
