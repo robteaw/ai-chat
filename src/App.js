0009165b2133dbda106bdd0503aad42b68f710bf
Robert Tea <robteaw@gmail.com>
2024-04-23 22:28:18 -0500

    logo and buttons added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <div className="upperSideTop">
+          <img src="" alt="" className="logo" />
+          <span className="brand">AiChat</span>
+          <button className="midBtn">
+            <img src="" alt="" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src="" alt="" />
+              What is programming?
+            </button>
+          </div>
+        </div>
+        <div className="lowerSide"></div>
+      </div>
+      <div className="main"></div>
+    </div>
+  );
 }
 
 export default App;
